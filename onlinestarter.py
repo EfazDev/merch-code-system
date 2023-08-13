9f51143a11e4bb3c31bd8d5a93a1fe13d91ae6f9
MegaSquadEfaz <efaz2019@icloud.com>
2023-08-13 16:24:49 -0500

    Update onlinestarter.py

--- onlinestarter.py
+++ onlinestarter.py
@@ -6,27 +6,41 @@ import os
 import time
     
 os.system("cls" if os.name == "nt" else "clear")
-    
-print("Welcome to Efaz's Merch Code System Launcher!")
-print("")
-print("Launcher Made by Efaz at efaz.dev")
-print("")
-print("Looking for System Device Details...")
-print("Detected: " + platform.system())
-print("Launching latest version...")
+
+def printSystemMessage(message):
+    print(f"\x1b[38;2;255;75;0m{message}\033[38;5;231m")
+
+def printMainMessage(mes):
+    print(f"\x1b[38;2;255;255;255m{mes}\033[38;5;231m")
+
+def printErrorMessage(mes):
+    print(f"\x1b[38;2;255;0;0m{mes}\033[38;5;231m")
+
+def printSuccessMessage(mes):
+    print(f"\x1b[38;2;0;255;0m{mes}\033[38;5;231m")
+
+printSystemMessage('''
+Welcome to Efaz's Merch Code System Launcher!
+Launcher Made by Efaz at efaz.dev
+
+Logs:
+''')
+printMainMessage("Looking for System Device Details...")
+printMainMessage("Detected: " + platform.system())
+printMainMessage("Launching latest version...")
 url = "https://raw.githubusercontent.com/EfazDev/merch-code-system/main/main.py"
 time.sleep(2)
-print("Locating Request from URL...")
+printMainMessage("Locating Request from URL...")
 resp = requests.get(url)
 if resp.status_code == 200:
-    print("Finished GET Request, saving script...")
+    printSuccessMessage("Finished GET Request, saving script...")
     content = resp.text
     with open("main.py", "w", encoding="utf-8") as f:
         f.write(content)
-    print("Finished Writing Script")
-    print("Running Script...")
+    printSuccessMessage("Finished Writing Script")
+    printMainMessage("Running Script...")
     subprocess.Popen([sys.executable, 'main.py'])
-    print("Finished Running, ending launcher...")
+    printSuccessMessage("Finished Running, ending launcher...")
     quit()
 else:
-    print("Server returned unknown status code. Extension not runned")
+    printErrorMessage("Server returned unknown status code. Merch Code System not ran")
